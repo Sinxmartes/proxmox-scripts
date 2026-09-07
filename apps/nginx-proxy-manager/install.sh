ff5b181372761707e4da9d167969df980e9c5be3
Marco "Sinxmartes" Medeiros <marco.medeiros@proton.me>
2026-09-06 21:18:58 -0300

    Update install.sh

--- apps/nginx-proxy-manager/install.sh
+++ apps/nginx-proxy-manager/install.sh
@@ -226,7 +226,7 @@ step_start "Nginx Proxy Manager" "Downloading" "Downloaded"
   echo "================="
   cd ./nginx-proxy-manager-$NPM_VERSION
   echo "PWD: $(pwd)"
-  sleed 100
+  sleep 100
   step_end "Nginx Proxy Manager ${CLR_CYB}v$NPM_VERSION${CLR} ${CLR_GN}Downloaded"
 
 step_start "Enviroment" "Setting up" "Setup"
